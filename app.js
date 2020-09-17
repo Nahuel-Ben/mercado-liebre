eb00528e095a4b214f1c9351cd7effbae351dca7
Nahuel-Ben <nahuel.b13@gmail.com>
2020-09-17 12:10:41 -0300

    primer commit de mercadoliebre

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require ('path');
+
+const app = express();
+
+app.get('/', function (req, res){
+    let file = path.resolve ('index.html');
+
+    res.sendFile(file);
+})
+
+app.get('*',function(req, res){
+    if(req.url.includes('.')){
+        let file = path.resolve('public' + req.url);
+        return res.sendFile(file);
+    }
+    res.send('Not found');
+})
+
+
+
+app.listen(5000);
