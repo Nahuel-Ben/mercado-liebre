94ced4fbd39f2089b59e8c25366c25b5e193ab4b
Nahuel-Ben <nahuel.b13@gmail.com>
2021-02-24 16:13:39 -0300

    edit productsController, views and routes

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
 const path = require('path');
-//const { Router } = require('express');
-const multer = require('multer');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
+const numberFormat = new Intl.toThousand('es-AR', {
+    style: 'currency',
+    currency: 'ARS',
+    minimumFractionDigits: 2
+  }) 
+
 const productsController = {
 	// Root - Show all products
 	productsList: (req, res) => {
+        products.map(i => i.price = numberFormat.format(i.price))
 		res.render('products', {products : products})},
 	// Detail - Detail from one product
 	detail: (req, res) => {
@@ -18,10 +23,10 @@ const productsController = {
 		let product = products.find((e) => e.id == req.params.id);
 
         if (product != undefined){
-            res.render('detail', {stylesheet: 'bootstrap-grid.min', product : products})
+            return res.render('detail', {stylesheet: 'bootstrap-grid.min', product : products})
         }
 
-        /* res.status(404).render('products/detail', {stylesheet: 'bootstrap-grid.min', product : error}) */
+        res.status(404).render('products/detail', {stylesheet: 'bootstrap-grid.min', product : error})
     
 	},
 
@@ -33,21 +38,56 @@ const productsController = {
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
-		res.send('Soy el store')
-	},
+		// Do the magic    
+                let productsList = [...products];
+                let addProduct = {
+	           id: productsList [productsList.length - 1].id + 1,
+                   ...req.body,
+                   image: req.files [0].filename
+                }
+                productsList.push (addProduct)
 
+                fs.writeFileSync ('./data/productsDataBase.json', JSON.stringify(productsList))
+
+                res.redirect ('/products')
+         },
 	// Update - Form to edit
 	edit: (req, res) => {
 		// Do the magic
-		res.send('Soy el edit')
+
+                let product = products.find (r => r.id == req.params.id);
+                  if (product != undefied) {
+		     return res.render ('product-edit-form', { title: 'Editar | Modificar producto', stylesheet: 'bootstrap-grid.min' })
+                  }
+
+                res.status (404).send ('No se encontró el producto')
+
 	},
 	// Update - Method to update
 	update: (req, res) => {
 		// Do the magic
 		
-	},
-
+                let productsList = [...products]
+                let product = productsList.find (r => r.id == req.params.id);
+                let image = image.avatar
+                   if (req.files [0] != undefined) {
+                   image = req.files[0].filename
+                   }
+     
+                 productsList =productsList.map (r => {
+                   if (r.id == product.id) {
+                      r = {
+                            id: product.id,
+                            ...req.body,
+                            image: image
+                           }
+                   }
+                  return r
+                  });
+               
+               fs.writeFileSync ('./data/productsDataBase.json', JSON.stringify(productsList))
+               res.redirect ('detail', {stylesheet: 'bootstrap-grid.min', product : products})
+        },
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
 		// Do the magic
--- src/routes/products.js
+++ src/routes/products.js
@@ -7,20 +7,33 @@ const path = require('path');
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/images/products')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+   
+var upload = multer({ storage: storage })
+
 /*** GET ALL PRODUCTS ***/ 
-router.get('/', productsController.productsList); 
+router.get('/', productsController.productsList);
+
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/create', productsController.create); 
-router.post('/', productsController.store); 
+router.post('/create',upload.any(), productsController.store);
 
 
 /*** GET ONE PRODUCT ***/ 
-router.get('/:id', productsController.detail); 
+router.get('/:id?', productsController.detail);
+ 
 
 /*** EDIT ONE PRODUCT ***/ 
 router.get('/edit/:id', productsController.edit); 
-router.put('/', productsController.update); 
+router.put('/edit/:id', productsController.update); 
 
 
 /*** DELETE ONE PRODUCT***/ 
